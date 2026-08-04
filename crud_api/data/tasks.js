4f8aa27c01f37406db13b7e5718184f4ab644448
Anuja khatri <khatrianuja20@gmail.com>
2026-08-04 23:00:02 +0545

    Stage 2: read endpoints with 404

---
+++ crud_api/data/tasks.js
@@ -0,0 +1,8 @@
+let tasks = [
+    {id: 1, title: "Learn Node.js", done: true},
+    {id: 2, title: "Learn Express.js", done: true},
+    {id: 3, title: "Learn Middleware", done: false},
+    {id: 4, title: "Learn MongoDB", done: false}
+]
+
+module.exports = tasks;
